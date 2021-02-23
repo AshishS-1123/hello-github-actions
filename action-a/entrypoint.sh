6b5e0ccf2dbfae085df83cf78816a4567b5bf7d7
Ashish Shevale <shevaleashish@gmail.com>
2021-02-23 13:19:15 +0530

    New

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/sh -l
 
 sh -c "echo Hello world my name is $INPUT_MY_NAME"
+
+sh -c "echo Installing Dependencies"
+pip install tqdm
+
+sh -c "echo Building Wheel"
+python setup.py bdist_wheel
+
+sh -c "Done building wheel"
